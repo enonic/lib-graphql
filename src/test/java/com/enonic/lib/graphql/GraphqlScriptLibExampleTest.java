99d4d3dcec591532fd5afed0f36dbebcaecf9684
Glenn Ricaud <gri@enonic.com>
2017-05-11 11:59:51 +0200

    README-md and example

---
+++ src/test/java/com/enonic/lib/graphql/GraphqlScriptLibExampleTest.java
@@ -0,0 +1,13 @@
+package com.enonic.lib.graphql;
+
+import com.enonic.xp.testing.script.ScriptRunnerSupport;
+
+public class GraphqlScriptLibExampleTest
+    extends ScriptRunnerSupport
+{
+    @Override
+    public String getScriptTestFile()
+    {
+        return "/lib/graphql-example.js";
+    }
+}
---
+++ src/test/resources/lib/graphql-example.js
@@ -0,0 +1,144 @@
+// Includes the library
+var graphQlLib = require('/lib/graphql');
+
+// For the simplicity of the example we will use a JS object as database.
+// In a real life case you will rather retrieve nodes/contents/data using one of the following libraries: '/lib/xp/content', 
+// '/lib/xp/node' or '/lib/sql'.
+var database = {
+    'James': {
+        name: 'James',
+        age: 42,
+        children: ['John', 'Robert']
+    },
+    'John': {
+        name: 'John',
+        age: 12,
+        children: []
+    },
+    'Robert': {
+        name: 'Robert',
+        age: 10,
+        children: []
+    }
+};
+
+// First defines the object types. 
+// In this case: an object type 'Person' with a mandatory String field 'name', a mandatory Integer field 'age' and an array of Person 
+// field 'children'.
+var personObjectType = graphQlLib.createObjectType({
+    name: 'Person',
+    description: 'A person type.',
+    fields: {
+        name: {
+            type: graphQlLib.nonNull(graphQlLib.GraphQLString),
+            resolve: function (env) {
+                //The source on which is executed this function is one of the 3 person database objects defined above. 
+                // We will simply return the field 'name' of this object here.
+                return env.source.name;
+            }
+        },
+        age: {
+            type: graphQlLib.nonNull(graphQlLib.GraphQLInt),
+            resolve: function (env) {
+                //The source on which is executed this function is one of the 3 person database objects defined above. 
+                // We will simply return the field 'age' of this object here.
+                return env.source.age;
+            }
+        },
+        children: {
+            // We use here a reference because the object type Person has not been defined yet.
+            // If it had been defined it would have been: graphQlLib.list(personObjectType)
+            type: graphQlLib.list(graphQlLib.reference('Person')),
+            resolve: function (env) {
+                //The source on which is executed this function is one of the 3 person database objects defined above. 
+                // We will use the field 'children' to retrieve the corresponding database objects.
+                return env.source.children.map(function (childName) {
+                    return database[childName];
+                });
+            }
+        }
+    }
+});
+
+// Defines a second object type 'Query', the root object type containing all the root retrieval requests.
+var rootQueryType = graphQlLib.createObjectType({
+    name: 'Query',
+    fields: {
+        //We have here a unique request getPersonByName taking a mandatory String 'name' as parameter and returning a person 
+        getPersonByName: {
+            type: personObjectType,
+            args: {
+                name: graphQlLib.nonNull(graphQlLib.GraphQLString)
+            },
+            resolve: function (env) {
+                var name = env.args.name;
+                return database[name];
+            }
+        }
+    }
+});
+
+// Define the GraphQL schema
+var schema = graphQlLib.createSchema({
+    query: rootQueryType
+});
+
+// Here is an example of a POST request handler that will execute the query received as parameter against the schema defined above.
+exports.post = function (req) {
+    var body = JSON.parse(req.body);
+    var result = graphQlLib.execute(schema, body.query, body.variables);
+    return {
+        contentType: 'application/json',
+        body: result
+    };
+};
+
+
+// Example of unit test for this service:
+// We send the following query: query($name:String!){getPersonByName(name:$name){name, age, children{name, age}}}
+// With the following variables: {name: 'James'}
+// The result is then:
+// {
+//   data: {
+//     getPersonByName: {
+//       name: 'James',
+//       age: 42,
+//       children: [
+//         {
+//           name: "John",
+//           age: 12
+//         },
+//         {
+//           name: "Robert",
+//           age: 10
+//         }
+//       ]
+//     }
+//   }
+// }
+var assert = require('/lib/xp/assert');
+exports.test = function () {
+    var query = 'query($name:String!){getPersonByName(name:$name){name, age, children{name, age}}}';
+    var variables = {name: 'James'};
+    var result = exports.post({
+        body: JSON.stringify({query: query, variables: variables})
+    }).body;
+    assert.assertJsonEquals({
+        data: {
+            getPersonByName: {
+                name: 'James',
+                age: 42,
+                children: [
+                    {
+                        name: "John",
+                        age: 12
+                    },
+                    {
+                        name: "Robert",
+                        age: 10
+                    }
+                ]
+            }
+        }
+    }, result);
+};
